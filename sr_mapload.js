e6a5313addbff4a25ca2b9631817a6d5ceb5977c
Jonathan Reifer <jonathanr@osrce.com>
2011-03-27 01:13:20 +0000

    Added some code for additional layers, demo for data_sensitive layer, will move code out to separte file soon.

--- sr_mapload.js
+++ sr_mapload.js
@@ -32,14 +32,20 @@ var gmap = new OpenLayers.Layer.Google( "Google Maps", {numZoomLevels: 20} );
 // Add the precinct boundaries as a gml file for now.
 var policePcts = new OpenLayers.Layer.GML("Precinct Boundaries", "data_public/PolicePctBoundaries.gml" ,{ isBaseLayer: false, projection: "EPSG:4326", visibility: false} );
 
+//Testing purposes only, we're going to move this to its own file soon.
+var stc_chokepoints = new OpenLayers.Layer.GML("NYPD STC Chokepoints", "data_sensitive/NYPD_STC_CHOKEPOINTS.gml" ,{ isBaseLayer: false, projection: "EPSG:4326", visibility: false} );
+var nypd_veh_inter_com = new OpenLayers.Layer.GML("NYPD Commercial Vehicle Interdiction", "data_sensitive/NYPD_VEH_INTERDICTION_COMMERCIAL.gml" ,{ isBaseLayer: false, projection: "EPSG:4326", visibility: false} );
+var nypd_veh_inter_pas = new OpenLayers.Layer.GML("NYPD Passenger Vehicle Interdiction", "data_sensitive/NYPD_VEH_INTERDICTION_PASSENGER.gml" ,{ isBaseLayer: false, projection: "EPSG:4326", visibility: false} );
 
 //Add style for precincts :
 
 
 
 map.addLayers( [  gmap  ]);
-
 map.addLayers( [   policePcts ]);
+//Add testing layers - to be taken out soon.
+map.addLayers( [ stc_chokepoints, nypd_veh_inter_com, nypd_veh_inter_pas ]);
+
 
 map.addControl(new OpenLayers.Control.LayerSwitcher() );
 //map.zoomToMaxExtent();
