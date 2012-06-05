af3c5d904fa65dff516296dc752362e3bcbd80d0
Jonathan Reifer <jonathanr@osrce.com>
2012-06-05 18:08:47 -0400

    Created Cfsupdatequeue files for new table sr_cfs_updatequeue which now gets single cfs added and NOTIFIED everytime we view cfssingle.

--- application/modules/srdata/models/DbTable/Cfs.php
+++ application/modules/srdata/models/DbTable/Cfs.php
@@ -5,6 +5,7 @@ class Srdata_Model_DbTable_Cfs extends Zend_Db_Table_Abstract
 
     protected $_name = 'sr_cfs_withlocation';
 		protected $_rowClass = 'Srdata_Model_Cfs';
-		protected $_primary = array('cfs_data', 'cfs_num');
+		protected $_primary = array('cfs_date', 'cfs_num');
+		protected $_sequence = false;
 }
 
